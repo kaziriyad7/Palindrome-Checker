ada288bbaf87b208f3bbd650fd05173724295f6b
Kazi Rasel <kazirasel50@gmail.com>
2022-03-17 01:08:23 +0600

    fixed input and button filed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 const textInput = document.querySelector(".inputs input");
+let checkBtn = document.querySelector(".inputs button");
+let filterInput;
+
+checkBtn.addEventListener('click', () => {
+    console.log(filterInput);
+});
 
 
 textInput.addEventListener("keyup", () => {
-    
+    filterInput = textInput.value.replace(/[^A-Z0-9]/ig,"");
+    if (filterInput) {
+        return checkBtn.classList.add("active");
+    }
+    checkBtn.classList.remove("active");
 });
