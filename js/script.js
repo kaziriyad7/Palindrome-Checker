08a6f3154e5092b5b8d1535cf5eecaee66aab665
Kazi Rasel <kazirasel50@gmail.com>
2022-03-17 01:24:26 +0600

    allmost complete my project

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
 const textInput = document.querySelector(".inputs input");
 let checkBtn = document.querySelector(".inputs button");
+let infoText = document.querySelector('.info-txt')
 let filterInput;
 
 checkBtn.addEventListener('click', () => {
-    console.log(filterInput);
+
+    let reverseInput = filterInput.split("").reverse().join("");
+    infoText.style.display = "block";
+    if (filterInput != reverseInput) {
+        return (infoText.innerHTML = `No, <span>'${textInput.value}'</span> isn't a palindrome!`);
+    }
+    infoText.innerHTML = `Yes, <span>'${textInput.value}'</span> is a palindrome!`;
 });
 
 
 textInput.addEventListener("keyup", () => {
-    filterInput = textInput.value.replace(/[^A-Z0-9]/ig,"");
+    filterInput = textInput.value.toLowerCase().replace(/[^A-Z0-9]/gi, "");
     if (filterInput) {
         return checkBtn.classList.add("active");
     }
+    infoText.style.display = "none";
     checkBtn.classList.remove("active");
 });
